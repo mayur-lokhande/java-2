f25511d32764ebeedd6cde2651aad05dcb967879
mayur-lokhande <mayurblokhande@gmail.com>
2024-04-29 18:09:51 +0530

    commit 12

--- LearningJava/src/oop/MethodOverloading.java
+++ LearningJava/src/oop/MethodOverloading.java
@@ -1,5 +1,33 @@
 package oop;
 
-public class MethodOverloading {
+class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
 
+	public int add(int a, int b, int c) {// different number of parameters
+		return a + b + c;
+	}
+
+	public int add(double a, double b) {// different type of parameters
+		return (int) (a + b);
+	}
+
+	public int add(double a, int b) {// different order of parameters
+		return (int) (a + b);
+	}
+}
+
+public class MethodOverloading {
+	public static void main(String[] args) {
+		Calculator calculator=new Calculator();
+		int result1 = calculator.add(22, 56);
+		System.out.println(result1);
+		int result2 =calculator.add(12, 34, 56);
+		System.out.println(result2);
+		int result3 =calculator.add(23.1, 45.6);
+		System.out.println(result3);
+		int result4 =calculator.add(34.2, 76);
+		System.out.println(result4);
+	}
 }
