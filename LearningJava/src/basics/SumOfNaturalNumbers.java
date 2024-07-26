8cec16040a663c27673cb7958fd97e8fec267034
mayur-lokhande <mayurblokhande@gmail.com>
2024-07-26 23:03:13 +0530

    new

--- LearningJava/src/basics/SumOfNaturalNumbers.java
+++ LearningJava/src/basics/SumOfNaturalNumbers.java
@@ -1,12 +1,25 @@
 package basics;
 
+import java.util.Scanner;
+
 public class SumOfNaturalNumbers {
 	public static void main(String[] args) {
-		int n=4;
+		Scanner scan = new Scanner(System.in);
+		
+		//addition of n natural numbers.
+		System.out.println("Enter value of n:");
+		int n=scan.nextInt();
 		int sum=0;
 		for(int i=1;i<=n;i++) {
 			sum+=i;
 		}
 		System.out.println(sum);
+		
+		//print the table of inputed value
+		System.out.println("Enter value of num:");
+		int num=scan.nextInt();
+		for(int j=1;j<=10;j++) {
+			System.out.println(num*j);
+		}
 	}
 }
